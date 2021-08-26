01ca68051abd4b0e0269f1815177d51625864a63
Tony-Kara <eniolaanthony@gmail.com>
2021-08-26 18:31:49 +0600

    Initial Commit

---
+++ Shared/CodeWithDesignAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeWithDesignAppApp.swift
+//  Shared
+//
+//  Created by mac on 8/26/21.
+//
+
+import SwiftUI
+
+@main
+struct CodeWithDesignAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
