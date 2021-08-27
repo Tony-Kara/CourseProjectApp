0a8b65fd439d196e6ad932eea1d381374ed443fe
Tony-Kara <eniolaanthony@gmail.com>
2021-08-27 23:23:59 +0600

    Add target membership for new class to ensure they can be found when running codes for Mac

--- Shared/SideBar.swift
+++ Shared/SideBar.swift
@@ -11,20 +11,30 @@ struct SideBar: View {
     var body: some View {
        
         NavigationView { // creates a navigattion view
-            List {
-                NavigationLink(destination: CoursesView()) {
-                    Label("Courses", systemImage: "book.closed")
-                }
-                Label("Tutorials", systemImage: "list.bullet.rectangle")
-                Label("Livestreams", systemImage: "tv")
-                Label("Certificates", systemImage: "mail.stack")
-                Label("Search", systemImage: "magnifyingglass")
-            }
-            .listStyle(SidebarListStyle())
-            .navigationTitle("Learn")
+            #if os(iOS)
+            content
+                .navigationTitle("Learn") // the navigation title is only appropriate for iOS and not mac
+            #else
+            content
+                .frame(minWidth: 200, idealWidth: 250, maxWidth: .infinity, maxHeight: .infinity)
+            #endif
             
-            CoursesView()
+            CoursesView() // adding this here ensures that my view displays these  contents  on first loading up.
         }
+        
+    }
+    
+    var content: some View {
+        List {
+            NavigationLink(destination: CoursesView()) {
+                Label("Courses", systemImage: "book.closed")
+            }
+            Label("Tutorials", systemImage: "list.bullet.rectangle")
+            Label("Livestreams", systemImage: "tv")
+            Label("Certificates", systemImage: "mail.stack")
+            Label("Search", systemImage: "magnifyingglass")
+        }
+        .listStyle(SidebarListStyle())
         
     }
 }
