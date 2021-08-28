833eea1a58bf1b57ce08bc1e70a86ad3a41a1c96
Tony-Kara <eniolaanthony@gmail.com>
2021-08-28 15:24:16 +0600

    Modify transition with animation to make it better

--- Shared/CoursesView.swift
+++ Shared/CoursesView.swift
@@ -9,26 +9,29 @@ import SwiftUI
 
 struct CoursesView: View {
     @State var show = false // add a state to either ensure a display of the second view or not
+    @Namespace var namespace // a namespace is where you set a collection of match elements
     var body: some View {
         ZStack {
             CourseItem()
+                .matchedGeometryEffect(id: "Card", in: namespace, isSource: !show) //always ensure you add the ".matchedGeometryEffect" before the frame
                 .frame(width: 335, height: 250)
             
             VStack {
                 
                 if show { // this is set to false by default and will not show by default
                     CourseItem()
-                        .transition(.move(edge: .leading)) // change this to set the direction your transition takes place from
+                        .matchedGeometryEffect(id: "Card", in: namespace)
+                        .transition(.opacity) // change this to set the direction your transition takes place from ie movement of your view
                         .edgesIgnoringSafeArea(.all)
                 }
             }
        
         }
-        .onTapGesture { //on tapping the card
+        .onTapGesture { //on tapping the card, we want a transition with animation
             
-            withAnimation(.spring()){ // the spring() gives it a nice boucing effect
+            withAnimation(.spring()){ // the spring() gives it a nice boucing effect, we set the kind of animation we want here
                 
-                show.toggle() // this will toggle a boolean value ie between false and true
+                show.toggle() // this will toggle a boolean value ie between false and true which makes the transtion occur
             }
            
         }
